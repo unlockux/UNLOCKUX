11bcd742cbaed420f9ad889827ce49fa4173ffdf
Jason Christie <jchristie85@gmail.com>
2021-02-23 16:35:54 +0000

    Cooper & updated colours

---
+++ cooper.php
@@ -0,0 +1,28 @@
+<?php include( 'config.php' ); ?>
+<?php
+$pageTitle = 'UNLOCK UX - Coopers day out';
+$pageDesc = 'A fun day out at Fairy Glen near Parbold with Cooper - 21st Feb 2021';
+$bodyColor = 'bg-trueblack';
+?>
+<?php include( INCLUDES_DIR . 'header-meta.php'); ?>
+
+<div class="fixed top-0 left-0">
+    <a href="/" class="white fas fa-home bg-blue white padding-30 "></a>
+</div>
+
+<div class="flex flex-wrap justify-center padding-10 overflow-hidden">
+        <img class="padding-10" p style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-01.jpg">
+        <img class="padding-10" p style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-02.jpg">
+        <img class="padding-10" l style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-13.jpg">
+        <img class="padding-10" l style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-14.jpg">
+        <img class="padding-10" l style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-05.jpg">
+        <img class="padding-10" p style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-03.jpg">
+        <img class="padding-10" p style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-07.jpg">
+        <img class="padding-10" l style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-08.jpg">
+        <img class="padding-10" l style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-09.jpg">
+        <img class="padding-10" p style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-10.jpg">
+        <img class="padding-10" p style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-11.jpg">
+        <img class="padding-10" l style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-12.jpg">
+        <img class="padding-10" l style="max-width:100%;height:100vh;object-fit:cover;" src="dist/images/cooper/Cooper-fairy-glen-04.jpg">
+</div>
+<?php include( INCLUDES_DIR . 'footer.php'); ?>
--- includes/header-meta.php
+++ includes/header-meta.php
@@ -6,10 +6,10 @@
     <meta charset="utf-8">
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <title>Unlock UX</title>
-    <meta name="description" content="Testing and building concepts to help better understand how they work on desktop and mobile">
+    <title><?php echo $pageTitle; ?></title>
+    <meta name="description" content="<?php echo $pageDesc; ?>">
     <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@800&family=Inter:wght@800&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/a277fdf1b2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="<?php echo $route; ?>dist/css/app.css" rel="stylesheet">
 </head>
-<body class="">
+<body class="<?php echo $bodyColor; ?>">
