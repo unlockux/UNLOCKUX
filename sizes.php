b05276f4479ebeb830d0ff75ec0dbbf3dc789b84
Jason <jchristie85@mail.com>
2021-03-03 08:46:20 +0000

    breakpoints view

---
+++ sizes.php
@@ -0,0 +1,29 @@
+<?php include( 'config.php' ); ?>
+<?php include( INCLUDES_DIR . 'header-meta.php'); ?>
+
+<div class="bg-purple height-v35 || flex justify-center items-center">
+    <h1 class="white">Sizes</h1>
+</div>
+
+<div class="bg-peach white" style="height:30px;width:992px">992px</div>
+<div class="bg-blue white" style="height:30px;width:1199px">1199px</div>
+
+
+<div class="row">
+    <div class="container flex flex-wrap || margin-x10-negative margin-t10">
+        <div class="small-12 medium-6 large-3 || padding-10">
+            <div class="height-v25 || bg-blue"></div>
+        </div>
+        <div class="small-12 medium-6 large-3 || padding-10">
+            <div class="height-v25 || bg-peach"></div>
+        </div>
+        <div class="small-12 medium-6 large-3 || padding-10">
+            <div class="height-v25 || bg-blue"></div>
+        </div>
+        <div class="small-12 medium-6 large-3 || padding-10">
+            <div class="height-v25 || bg-peach"></div>
+        </div>
+    </div>
+</div>
+
+<?php include( INCLUDES_DIR . 'footer.php'); ?>
