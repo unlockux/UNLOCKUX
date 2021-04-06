aa0d484d32e0b187bad17b00a295bf1a9aceb1fc
Jason Christie <jchristie85@gmail.com>
2021-04-06 14:46:27 +0100

    CSM

---
+++ csm.php
@@ -0,0 +1,25 @@
+<?php include( 'config.php' ); ?>
+<?php include( INCLUDES_DIR . 'header-meta.php'); ?>
+
+<div class="row flex justify-center || padding-y50">
+    <span>CSM</span>
+</div>
+
+<div class="container bg-blue flex justify-center">
+<div class="row padding-0 margin-gutter-xn || flex flex-wrap || bg-peach">
+
+<!-- CARD -->
+
+<?php for($i=1; $i<=4; $i++):?>
+<div class="large-4 padding-gutter">
+    <div class="border border-grey2 radius-small || padding-m">
+        <p class="margin-0">Wayfarers retro live-edge, 3 wolf moon cloud bread shoreditch shaman plaid mustache succulents banh mi venmo. Keffiyeh stumptown ugh plaid marfa.</p>
+    </div>
+</div>
+<?php endfor;?>
+
+</div>
+</div>
+
+
+<?php include( INCLUDES_DIR . 'footer.php'); ?>
---
+++ jen.php
@@ -0,0 +1,14 @@
+<?php include( 'config.php' ); ?>
+<?php include( INCLUDES_DIR . 'header-meta.php'); ?>
+
+<div class="row flex justify-center || padding-y50 || bg-grey">
+    <span>Jen</span>
+</div>
+
+<div class="row flex justify-center || padding-y50 || bg-grey">
+    <ul class="list-reset">
+        <li><a href="/" class="">Home</a></li>
+    </ul>
+</div>
+
+<?php include( INCLUDES_DIR . 'footer.php'); ?>
